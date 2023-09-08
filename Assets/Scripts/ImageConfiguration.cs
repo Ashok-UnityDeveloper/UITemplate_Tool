804f0acd494d356faee304dfdede5bad693da471
Ashok-UnityDeveloper <ashokdevara01@gmail.com>
2023-09-08 16:11:04 +0530

    Initial commit

---
+++ Assets/Scripts/ImageConfiguration.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "ImageConfig", menuName = "UI/Image Configuration")]
+public class ImageConfiguration : ScriptableObject
+{
+    public Sprite backGroundImageSprite;
+    public Sprite adIconSprite;
+    
+}
+
